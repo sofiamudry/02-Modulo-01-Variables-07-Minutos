4b048fb300f3db9a522cafd2695c0538e8563c7e
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 12:43:33 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Minutos a Segundos');
+
+alert('Hola, Binvenide!');
+
+const minutos = prompt('Ingrese los minutos');
+const minutosASegundos = (minutos*60);
+
+alert(`Los minutos ingresados son: ${minutosASegundos} segundos`);
